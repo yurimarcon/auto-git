6db0cd1e10aafc6ffdd078b7ef18b11a55c47968
yurimarcon <yurimarconoliveira@gmail.com>
2024-06-13 16:35:15 -0300

    Add delete function and create a menu

--- auto-git.sh
+++ auto-git.sh
@@ -10,6 +10,7 @@ function exit_exception (){
 function switch_branch (){
     
     selected=$(git branch | fzf +m \
+        --header "Select the branch to go:" \
         --height 40% \
         --layout reverse \
         --border\
@@ -27,6 +28,7 @@ function switch_branch (){
 function merge (){
 
     selected=$(git branch | fzf +m \
+        --header "Select the branch to merge:" \
         --height 100% \
         --layout reverse \
         --border\
@@ -44,6 +46,7 @@ function merge (){
 function delete_branch (){
     
     selected=$(git branch | fzf +m \
+        --header "select the branch to delete:" \
         --height 40% \
         --layout reverse \
         --border\
@@ -58,5 +61,47 @@ function delete_branch (){
     git branch -d "$selected"
 }
 
-delete_branch
+function main (){
+    
+    options=(\
+        "1 - Switch branch" \
+        "2 - Git merge" \
+        "3 - Delete branch" \
+        "Exit"\
+        )
 
+    selected=$( for opt in "${options[@]}" ; do echo $opt ; done | fzf +m \
+        --header "Select one option:" \
+        --height 40% \
+        --layout reverse \
+        --border\
+        --color bg:#222222)
+
+    exit_exception
+    
+    case "$selected" in
+        ${options[0]})
+            echo "$selected"
+            switch_branch
+            exit 0
+        ;;
+        ${options[1]})
+            echo "$selected"
+            merge
+            exit 0
+        ;;
+        ${options[2]})
+            echo "$selected"
+            delete_branch
+            exit 0
+        ;;
+        ${options[3]})
+            echo "$selected"
+            exit 0
+        ;;
+        *)
+        exit 0
+    esac
+}
+
+main
