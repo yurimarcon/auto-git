868f2cd9dc2d9bc8f7c575e6be244199838c9102
yurimarcon <yurimarconoliveira@gmail.com>
2024-06-14 11:39:14 -0300

    Updating install file

--- install.sh
+++ install.sh
@@ -19,21 +19,42 @@ function validate_local_dir (){
 
 }
 
+function update_bashrc (){
+
+    res=$(grep '"ag"=' ~/.bashrc)
+
+    if [ -z "$res" ]; then
+        echo "alias \"ag\"=\"~/.local/bin/auto-git\"" >> ~/.bashrc
+    else
+        echo "You alredy have a alias called 'ag', if it not is to your auto-git, please configure manualy."
+    fi
+    
+}
+
 function install (){
 
     validate_local_dir 
 
     cat ~/.local/auto-git/auto-git.sh > ~/.local/bin/auto-git
 
+    chmod +x ~/.local/bin/auto-git
+
+    update_bashrc 
+
     sudo rm -r ~/.local/auto-git
 
 }
 
-if [ -f ~/.local/bin/auto-git ]; then
-    echo "Upgrading..."
-    install
-else
-    echo "Instaling..."
-    install
-fi
+function main () {
 
+    if [ -f ~/.local/bin/auto-git ]; then
+        echo "Upgrading..."
+        install
+    else
+        echo "Instaling..."
+        install
+    fi
+
+}
+
+main
